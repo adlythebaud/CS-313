014ce284af114b64337ce5b627aff95c57308be1
Adly Thebaud <adlythebaud@gmail.com>
2019-05-13 21:46:14 -0600

    testing a deployment using Azure DevOps

--- web/assignments/week_03/prove_03/browse.php
+++ web/assignments/week_03/prove_03/browse.php
@@ -6,14 +6,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tiger Tongues Footwear</title>
+    <title>Fire 🔥 Footwear</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="jumbotron">
-        <h1 class="d-flex justify-content-center">Welcome to Tiger Tongues Footwear.</h1>
+        <h1 class="d-flex justify-content-center">Welcome to Fire 🔥 Footwear.</h1>
+        <h4 class="d-flex justify-content-center">Add some heat to your sneaker game.</h4>
     </div>
 
     <div id="shoes">
@@ -33,33 +34,30 @@
         $username = getenv("USERNAME");
         $password = getenv("PASSWORD");
         $database = getenv("DATABASE");
-        
-        
 
         $conn = mysqli_connect($hostname, $username, $password, $database);
 
         // Check connection
         if (!$conn) {
-
             die("Connection failed: " . mysqli_connect_error());            
-
         }
         // get the items to show:
-        $display_items = "SELECT DISTINCT shoe_id, shoe_name, price FROM my_maria_db.website_inventory";
+        $display_items = "SELECT DISTINCT shoe_id, shoe_name, price, shoe_description FROM my_maria_db.website_inventory";
         $result = $conn->query($display_items);
 
         if ($result->num_rows > 0) {
             // output data of each row
-            while($row = $result->fetch_assoc()) {
-                
-                echo "<img src=\"./images/" . $row["shoe_id"] . "_01.jpg\" alt=" . "\"" . $row["shoe_id"] . "\" class=\"img-thumbnail\" height=\"200\" width=\"200\"> <br>"; 
-                echo "<strong>" . $row["shoe_name"] . "</strong><br>Price: $" . $row["price"] . "<br>";
+            while($row = $result->fetch_assoc()) {                
+                echo "<img src=\"./images/" . $row["shoe_id"] . "_01.jpg\" alt=" . "\"" . 
+                $row["shoe_id"] . "\" class=\"img-thumbnail\" height=\"200\" width=\"200\">
+                <br>
+                <strong>" . $row["shoe_name"] . "</strong><br>$" . $row["price"] . "<br>";                
             }
         } else {
-            echo "0 results";
+            echo "Our store is empty! Check back soon for new stock.";
         }
         $conn->close();
     ?>
 </body>
-
+<script src="./scripts/browse.js"></script>
 </html>

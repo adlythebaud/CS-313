d1ce7545fdc52e7ba6264d99f1850aeb66f981b1
Adly Thebaud <adlythebaud@gmail.com>
2019-05-16 00:36:53 -0600

    Prepping to use JS to populate UI and PHP to pass data back and forth

--- web/assignments/week_03/prove_03/browse.php
+++ web/assignments/week_03/prove_03/browse.php
@@ -1,4 +1,6 @@
-<?php include "../../../menu.php"; ?>
+<?php 
+include "../../../menu.php"; 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,6 +11,7 @@
     <title>Fire 🔥 Footwear</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/browse.css">
 </head>
 
 <body>
@@ -17,7 +20,7 @@
         <h4 class="d-flex justify-content-center">Add some heat to your sneaker game.</h4>
     </div>
 
-    <div id="shoes">
+    <div>
         <!-- 
             Fill this dynamically with shoes. 
             A shoe item is:
@@ -30,11 +33,15 @@
     <?php
         
         // use these in production.
-        $hostname = getenv("HOSTNAME");
-        $username = getenv("USERNAME");
-        $password = getenv("PASSWORD");
-        $database = getenv("DATABASE");
+        // $hostname = getenv("HOSTNAME");
+        // $username = getenv("USERNAME");
+        // $password = getenv("PASSWORD");
+        // $database = getenv("DATABASE");
 
+        $hostname = "mymariadb.ccswpeqyo8av.us-east-2.rds.amazonaws.com";
+        $username = "master";
+        $password = "02x9sWJoadGKYdIfP38";
+        $database = "my_maria_db";
         
         $conn = mysqli_connect($hostname, $username, $password, $database);
 
@@ -47,18 +54,43 @@
         $result = $conn->query($display_items);
 
         if ($result->num_rows > 0) {
+            echo '<form action="' . $_SERVER["PHP_SELF"]. '" method="POST">';
+            echo '<div class="container" id="shoes">';
             // output data of each row
             while($row = $result->fetch_assoc()) {                
-                echo "<img src=\"./images/" . $row["shoe_id"] . "_01.jpg\" alt=" . "\"" . 
-                $row["shoe_id"] . "\" class=\"img-thumbnail\" height=\"200\" width=\"200\">
-                <br>
-                <strong>" . $row["shoe_name"] . "</strong><br>$" . $row["price"] . "<br>";                
+                
+                // I should use JS or React to return this, but I should use echo to return values from the DB to be sent to my javascript file.
+                echo '<div class="card col-3" align="center">
+                        <img class="img-thumbnail shoe-img" src="./images/'. $row["shoe_id"] . '_01.jpg" alt="Card image cap" height="200" width="200">
+                        <div class="card-body">
+                            <h5 class="card-title">' . $row["shoe_name"] . '</h5>
+                            <p class="card-text">' . $row["shoe_description"] . '</p>
+                            <input type="hidden" name="shoes[]" value="' . $row["shoe_id"] . '">
+                            <input type="button" value="Add To Cart" class="shoe-input">
+                        </div>
+                </div>';
             }
+            echo '</div>';
+            echo '</form>';
+            
         } else {
             echo "Our store is empty! Check back soon for new stock.";
         }
+        
+        // if (!empty($_POST["shoe"])){
+        //     echo "<strong>Shoe ID: </strong>". $_POST["shoe"] . "<br>";
+        // }
+        
+        
         $conn->close();
     ?>
+
+    <?php 
+       session_start();
+       echo "does this work?"; 
+    ?>
+    <script src="./scripts/browse.js"></script>
 </body>
-<script src="./scripts/browse.js"></script>
+
+
 </html>
--- web/assignments/week_03/prove_03/scripts/browse.js
+++ web/assignments/week_03/prove_03/scripts/browse.js
@@ -1,3 +1,24 @@
-function display_items() {
+'use strict';
+
+console.log("working");
+
+var shoes = document.querySelectorAll(".shoe-input");
+console.log(shoes);
+
+shoes.forEach(function (item) {
+  item.addEventListener("click", function () {
     
-}
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);    
+      }
+    };
+    xhttp.open("POST", "./browse.php", true);
+
+    xhttp.setRequestHeader("shoe", item.previousElementSibling.value);
+
+    xhttp.send("shoe="+item.previousElementSibling.value);
+  });
+});

b41348eb50e122a3dc47c9bdad0353bb505fcd4f
Adly Thebaud <adlythebaud@gmail.com>
2019-05-16 00:36:53 -0600

    Prepping to use JS to populate UI and PHP to pass data back and forth

--- web/assignments/week_03/prove_03/browse.php
+++ web/assignments/week_03/prove_03/browse.php
@@ -1,4 +1,5 @@
 <?php 
+<<<<<<< refs/remotes/heroku/master
     include "../../../menu.php";
     
     session_start();
@@ -15,6 +16,9 @@
     }
     // var_dump($_SESSION["shoes"]);
     
+=======
+include "../../../menu.php"; 
+>>>>>>> Prepping to use JS to populate UI and PHP to pass data back and forth
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,8 +30,11 @@
     <title>Fire 🔥 Footwear</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<<<<<<< refs/remotes/heroku/master
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
         integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+=======
+>>>>>>> Prepping to use JS to populate UI and PHP to pass data back and forth
     <link rel="stylesheet" href="./styles/browse.css">
 </head>
 
@@ -37,6 +44,7 @@
         <h4 class="d-flex justify-content-center">Add some heat to your sneaker game.</h4>
     </div>
 
+<<<<<<< refs/remotes/heroku/master
     <div id="shopping-cart-container" class="row col-2">
         <a href="cart.html">
             <div id="shopping-cart"></div>
@@ -46,8 +54,73 @@
     <div id="shoes">
 
     </div>
+=======
+    <div>
+        <!-- 
+            Fill this dynamically with shoes. 
+            A shoe item is:
+                - An image
+                - Bold title
+                - Price
+                - Button to add a size to your shopping cart -> store the shoe_id, size, and price in a session variable to add to cart and then to be purchased.
+         -->
+    </div>
+    <?php
+        
+        // use these in production.
+        // $hostname = getenv("HOSTNAME");
+        // $username = getenv("USERNAME");
+        // $password = getenv("PASSWORD");
+        // $database = getenv("DATABASE");
+
+        $hostname = "mymariadb.ccswpeqyo8av.us-east-2.rds.amazonaws.com";
+        $username = "master";
+        $password = "02x9sWJoadGKYdIfP38";
+        $database = "my_maria_db";
+        
+        $conn = mysqli_connect($hostname, $username, $password, $database);
+>>>>>>> Prepping to use JS to populate UI and PHP to pass data back and forth
 
 
+<<<<<<< refs/remotes/heroku/master
+=======
+        if ($result->num_rows > 0) {
+            echo '<form action="' . $_SERVER["PHP_SELF"]. '" method="POST">';
+            echo '<div class="container" id="shoes">';
+            // output data of each row
+            while($row = $result->fetch_assoc()) {                
+                
+                // I should use JS or React to return this, but I should use echo to return values from the DB to be sent to my javascript file.
+                echo '<div class="card col-3" align="center">
+                        <img class="img-thumbnail shoe-img" src="./images/'. $row["shoe_id"] . '_01.jpg" alt="Card image cap" height="200" width="200">
+                        <div class="card-body">
+                            <h5 class="card-title">' . $row["shoe_name"] . '</h5>
+                            <p class="card-text">' . $row["shoe_description"] . '</p>
+                            <input type="hidden" name="shoes[]" value="' . $row["shoe_id"] . '">
+                            <input type="button" value="Add To Cart" class="shoe-input">
+                        </div>
+                </div>';
+            }
+            echo '</div>';
+            echo '</form>';
+            
+        } else {
+            echo "Our store is empty! Check back soon for new stock.";
+        }
+        
+        // if (!empty($_POST["shoe"])){
+        //     echo "<strong>Shoe ID: </strong>". $_POST["shoe"] . "<br>";
+        // }
+        
+        
+        $conn->close();
+    ?>
+
+    <?php 
+       session_start();
+       echo "does this work?"; 
+    ?>
+>>>>>>> Prepping to use JS to populate UI and PHP to pass data back and forth
     <script src="./scripts/browse.js"></script>
 </body>
 
--- web/assignments/week_03/prove_03/scripts/browse.js
+++ web/assignments/week_03/prove_03/scripts/browse.js
@@ -1,5 +1,6 @@
 'use strict';
 
+<<<<<<< refs/remotes/heroku/master
 var shoeContainer = document.querySelector('#shoes');
 var cartIcon = document.querySelector("#shopping-cart");
 var cartCount = 0;
@@ -38,3 +39,27 @@ xhttp.onreadystatechange = function () {
 
 xhttp.open("get", "get_data.php", true);
 xhttp.send();
+=======
+console.log("working");
+
+var shoes = document.querySelectorAll(".shoe-input");
+console.log(shoes);
+
+shoes.forEach(function (item) {
+  item.addEventListener("click", function () {
+    
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);    
+      }
+    };
+    xhttp.open("POST", "./browse.php", true);
+
+    xhttp.setRequestHeader("shoe", item.previousElementSibling.value);
+
+    xhttp.send("shoe="+item.previousElementSibling.value);
+  });
+});
+>>>>>>> Prepping to use JS to populate UI and PHP to pass data back and forth

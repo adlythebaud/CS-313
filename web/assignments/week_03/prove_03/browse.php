7384498b70af7c1d4010455b357b5ad1ac14b816
Adly Thebaud <adlythebaud@gmail.com>
2019-05-18 23:42:06 -0600

    changing store front UI

--- web/assignments/week_03/prove_03/browse.php
+++ web/assignments/week_03/prove_03/browse.php
@@ -23,7 +23,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fire 🔥 Footwear</title>
+    <title>🔥 Fire Footwear</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
@@ -33,7 +33,7 @@
 
 <body>
     <div class="jumbotron">
-        <h1 class="d-flex justify-content-center">Welcome to Fire 🔥 Footwear.</h1>
+        <h1 class="d-flex justify-content-center">Welcome to Fire Footwear.</h1>
         <h4 class="d-flex justify-content-center">Add some heat to your sneaker game.</h4>
     </div>
 

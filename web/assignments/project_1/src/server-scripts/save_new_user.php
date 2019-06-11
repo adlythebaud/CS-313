d913b292d9ea15143b04cb6b2756e926cf6f1084
Adly Thebaud <adlythebaud@gmail.com>
2019-06-11 15:22:32 -0600

    saving new users

--- web/assignments/project_1/src/server-scripts/save_new_user.php
+++ web/assignments/project_1/src/server-scripts/save_new_user.php
@@ -8,24 +8,24 @@
     
     try {
                 
-        // if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["first_name"] && $_POST["last_name"] && $_POST["email_address"] && $_POST["username"] && $_POST["password"] && $_POST["password_confirm"]) {
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["first_name"] && $_POST["last_name"] && $_POST["email_address"] && $_POST["username"] && $_POST["password"] && $_POST["password_confirm"]) {
             
-        //     // create user. Send correct response back.
+            // create user. Send correct response back.
             
-        //     $insert = "INSERT INTO project_1.users (user_id, first_name, last_name, username, password, email_address) VALUES (DEFAULT, " . $conn->quote($_POST["first_name"]) . ", " . $conn->quote($_POST["last_name"]) . ", " . $conn->quote($_POST["username"]) . ", " . $conn->quote(password_hash($_POST["password"], PASSWORD_DEFAULT)) . ", " . $conn->quote($_POST["email_address"]) . ")";
+            $insert = "INSERT INTO project_1.users (user_id, first_name, last_name, username, password, email_address) VALUES (DEFAULT, " . $conn->quote($_POST["first_name"]) . ", " . $conn->quote($_POST["last_name"]) . ", " . $conn->quote($_POST["username"]) . ", " . $conn->quote(password_hash($_POST["password"], PASSWORD_DEFAULT)) . ", " . $conn->quote($_POST["email_address"]) . ")";
 
             
-        //     if ($conn->exec($insert) === false) {
-        //         echo "0";
-        //     } else {
-        //         echo "1";
-        //         unset($insert);
-        //         unset($conn);
-        //     }
+            if ($conn->exec($insert) === false) {
+                echo "0";
+            } else {
+                echo "1";
+                unset($insert);
+                unset($conn);
+            }
 
             
 
-        // }
+        }
 
         echo "1";
         
